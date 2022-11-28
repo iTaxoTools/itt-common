a4eac6eb70b25e8d1232db196c0da2100461782d
StefanPatman <stefanpatman91@gmail.com>
2022-11-28 20:09:10 +0200

    Wrap __iter__ for AttrDict

--- src/itaxotools/common/utility.py
+++ src/itaxotools/common/utility.py
@@ -7,6 +7,9 @@ class AttrDict(dict):
         super().__init__(*args, **kwargs)
         self.__dict__ = self
 
+    def __iter__(self):
+        return iter(self.values())
+
 
 def override(f):
     return f
