2e3df701077cb20c7f66bddaad17eab9432cfe61
StefanPatman <stefanpatman91@gmail.com>
2023-11-29 18:52:32 +0200

    Move project info to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,46 +1,18 @@
-"""The setup module for iTaxoTools-common"""
-
-# Always prefer setuptools over distutils
-import pathlib
+"""A setuptools based setup module."""
 
 from setuptools import find_namespace_packages, setup
 
-# Get the long description from the README file
-here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / 'README.md').read_text(encoding='utf-8')
-
-
 setup(
-    name='itaxotools-common',
-    version='0.3.1',
-    description='Common resources for iTaxoTools',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Patmanidis Stefanos',
-    author_email='stefanpatman91@gmail.com',
     package_dir={'': 'src'},
+    include_package_data=True,
     packages=find_namespace_packages(
         include=('itaxotools*',),
         where='src',
     ),
-    python_requires='>=3.10.2, <4',
-    extras_require={
-        'gui': ['pyside6>=6.6.0'],
-        'dev': ['pyinstaller>=4.5.1', 'pytest', 'pytest-qt'],
-    },
     entry_points={
         'pyinstaller40': [
             'hook-dirs = itaxotools.__pyinstaller:get_hook_dirs',
             'tests = itaxotools.__pyinstaller:get_PyInstaller_tests'
         ]
     },
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)', # noqa
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3 :: Only',
-    ],
-    include_package_data=True,
 )
